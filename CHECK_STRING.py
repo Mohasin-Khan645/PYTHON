01dbd8e8eef5ff411c88493117907079aca531cf
Mohammad Mohasin Khan <kmohasin645@gmail.com>
2025-08-24 22:23:42 +0530

    CHECK_STRING.py

---
+++ CHECK_STRING.py
@@ -0,0 +1,8 @@
+txt = "think out of the box to become a developer"
+print("become" in txt)
+
+
+
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
